e0843aa81b91e6c9f129aa2dedb89fc885155ff7
tanishqsaraswat34 <151999977+tanishqsaraswat34@users.noreply.github.com>
2025-09-14 12:26:43 +0530

    Add files via upload

---
+++ fundMe.sol
@@ -0,0 +1,77 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./priceConverter.sol";
+
+error NotOwner();
+
+contract FundMe{
+    using PriceConverter for uint256;
+    mapping (address=>uint256) addressToAmount;
+    address[] public funders;
+    address public immutable i_owner;
+    uint256 public constant MinimumUsd=5*10**18; //5 USD
+    constructor(){
+        i_owner=msg.sender;
+    }	
+
+    function funding() public payable {
+        require(msg.value>= MinimumUsd, "not send enough amount");
+        addressToAmount[msg.sender]+= msg.value;
+        funders.push(msg.sender);
+
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed= AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version(); 
+    }
+
+    modifier onlyOwner(){
+        if (msg.sender!=i_owner) {
+            revert NotOwner();
+
+        }
+        _;
+    }
+    
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        funding();
+    }
+
+    receive() external payable {
+        funding();
+    }
+
+}
